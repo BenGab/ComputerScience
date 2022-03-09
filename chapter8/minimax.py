4ec10a35d0dfe29b5a5cd12b0ef289f511d6cc00
Gabor Benko <gabor.benko@silabs.com>
2022-03-09 16:31:09 +0100

    alpha beta pruning

--- chapter8/minimax.py
+++ chapter8/minimax.py
@@ -17,6 +17,26 @@ def minimax(board: Board, maximizing: bool, original_player: Piece, max_depth: i
         worst_eval = min(result, worst_eval)
         return worst_eval 
 
+def alphabeta(board: Board, maximizing: bool, original_player: Piece, max_depth: int = 8, alpha: float = float("-inf"), beta: float = float("inf")) -> float:
+    if board.is_win or board.is_draw or max_depth == 0:
+        return board.evaluate(original_player)
+    
+    if maximizing:
+        for move in board.legal_moves:
+            result: float = alphabeta(board.move(move), False, original_player, max_depth -1, alpha, beta)
+            alpha = max(result, alpha)
+            if beta <= alpha:
+                break
+        return alpha
+    else:
+        for move in board.legal_moves:
+            result = alpha(board.move(move), True, original_player, max_depth - 1, alpha, beta)
+            beta = min(result, beta)
+            if beta <= alpha:
+                break
+        return beta
+    
+
 def find_best_move(board: Board, max_depth: int = 8) -> Move:
     best_eval: float = float('-inf')
     best_move: Move = Move(-1)
